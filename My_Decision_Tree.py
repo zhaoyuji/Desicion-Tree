078d00de86c8a582235d5897c7c9b592e4f2fe2a
zhaoyuji <740399003@qq.com>
2018-03-23 11:06:57 +0800

    Add files via upload

---
+++ My_Decision_Tree.py
@@ -0,0 +1,262 @@
+from numpy import *
+import numpy as np
+import pandas as pd
+from math import log
+import operator
+import matplotlib.pyplot as plt
+import re
+
+##decision tree
+# calculate the shannon entropy
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+# split the dataset for discrete variables
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+# split the dataset for continuous variables
+def splitContinuousDataSet(dataSet, axis, value, direction):
+    retDataSet = []
+    for featVec in dataSet:
+        if direction == 0:
+            if featVec[axis] > value:
+                reducedFeatVec = featVec[:axis]
+                reducedFeatVec.extend(featVec[axis + 1:])
+                retDataSet.append(reducedFeatVec)
+        else:
+            if featVec[axis] <= value:
+                reducedFeatVec = featVec[:axis]
+                reducedFeatVec.extend(featVec[axis + 1:])
+                retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet, labels):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    bestSplitDict = {}
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        if type(featList[0]).__name__ == 'float' or type(featList[0]).__name__ == 'int':
+            sortfeatList = sorted(featList)
+            splitList = []
+            for j in range(len(sortfeatList) - 1):
+                splitList.append((sortfeatList[j] + sortfeatList[j + 1]) / 2.0)
+
+            bestSplitEntropy = 10000
+            slen = len(splitList)
+            for j in range(slen):
+                value = splitList[j]
+                newEntropy = 0.0
+                subDataSet0 = splitContinuousDataSet(dataSet, i, value, 0)
+                subDataSet1 = splitContinuousDataSet(dataSet, i, value, 1)
+                prob0 = len(subDataSet0) / float(len(dataSet))
+                newEntropy += prob0 * calcShannonEnt(subDataSet0)
+                prob1 = len(subDataSet1) / float(len(dataSet))
+                newEntropy += prob1 * calcShannonEnt(subDataSet1)
+                if newEntropy < bestSplitEntropy:
+                    bestSplitEntropy = newEntropy
+                    bestSplit = j
+            bestSplitDict[labels[i]] = splitList[bestSplit]
+            infoGain = baseEntropy - bestSplitEntropy
+        else:
+            uniqueVals = set(featList)
+            newEntropy = 0.0
+            for value in uniqueVals:
+                subDataSet = splitDataSet(dataSet, i, value)
+                prob = len(subDataSet) / float(len(dataSet))
+                newEntropy += prob * calcShannonEnt(subDataSet)
+            infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    if type(dataSet[0][bestFeature]).__name__ == 'float' or type(dataSet[0][bestFeature]).__name__ == 'int':
+        bestSplitValue = bestSplitDict[labels[bestFeature]]
+        labels[bestFeature] = labels[bestFeature] + '<=' + str(bestSplitValue)
+        for i in range(shape(dataSet)[0]):
+            if dataSet[i][bestFeature] <= bestSplitValue:
+                dataSet[i][bestFeature] = 1
+            else:
+                dataSet[i][bestFeature] = 0
+    return bestFeature
+
+# vote
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    return max(classCount)
+
+# main function
+def createTree(dataSet, labels, data_full, labels_full):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet, labels)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    if type(dataSet[0][bestFeat]).__name__ == 'str':
+        currentlabel = labels_full.index(labels[bestFeat])
+        featValuesFull = [example[currentlabel] for example in data_full]
+        uniqueValsFull = set(featValuesFull)
+    del (labels[bestFeat])
+    for value in uniqueVals:
+        subLabels = labels[:]
+        if type(dataSet[0][bestFeat]).__name__ == 'str':
+            uniqueValsFull.remove(value)
+        myTree[bestFeatLabel][value] = createTree(splitDataSet \
+                                                      (dataSet, bestFeat, value), subLabels, data_full, labels_full)
+    if type(dataSet[0][bestFeat]).__name__ == 'str':
+        for value in uniqueValsFull:
+            myTree[bestFeatLabel][value] = majorityCnt(classList)
+    return myTree
+
+#classify
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    if '<=' in firstStr:
+        featvalue = float(re.compile("(<=.+)").search(firstStr).group()[2:])
+        featkey = re.compile("(.+<=)").search(firstStr).group()[:-2]
+        secondDict = inputTree[firstStr]
+        featIndex = featLabels.index(featkey)
+        if testVec[featIndex] <= featvalue:
+            judge = 1
+        else:
+            judge = 0
+        for key in secondDict.keys():
+            if judge == int(key):
+                if type(secondDict[key]).__name__ == 'dict':
+                    classLabel = classify(secondDict[key], featLabels, testVec)
+                else:
+                    classLabel = secondDict[key]
+    else:
+        secondDict = inputTree[firstStr]
+        featIndex = featLabels.index(firstStr)
+        for key in secondDict.keys():
+            if testVec[featIndex] == key:
+                if type(secondDict[key]).__name__ == 'dict':
+                    classLabel = classify(secondDict[key], featLabels, testVec)
+                else:
+                    classLabel = secondDict[key]
+    return classLabel
+
+
+##plot the tree without pydot
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.savefig('myTree.png')
+    plt.show()
+
+#classification xigua.csv
+df = pd.read_csv('xigua.csv')
+idx = list(range(len(df)))
+np.random.shuffle(idx)
+df = df.loc[idx]
+df_train = df[:-2]
+df_test = df[-2:]
+data_train = df_train.values[:, 1:].tolist()
+data_train_full = data_train[:]
+labels = df.columns.values[1:-1].tolist()
+labels_full = labels[:]
+#training
+myTree = createTree(data_train, labels, data_train_full, labels_full)
+print myTree
+#plot the tree and write 'myTree.png'
+graph = createPlot(myTree)
+df_test = df[-2:]
+data_test = df_test.values[:, 1:].tolist()
+pred_y = []
+for i in range(len(data_test)):
+    pred_y.append(classify(myTree, labels_full, data_test[i]))
+y_test = df_test['label_haogua'].tolist()
+#compare the predicted value and real labels
+print('predictions: ', pred_y)
+print('real labels: ', y_test)
